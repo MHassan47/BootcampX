7956b41a2e2e506e225526ef9fc3b636e8829bef
Mohamed <mhassankd35@gmail.com>
2022-06-10 21:45:15 -0500

    add parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,8 @@
 const { Pool } = require("pg");
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
   user: "labber",
@@ -20,17 +24,17 @@ LIMIT 5;
   })
   .catch((err) => console.error("query error", err.stack));
 
-// Query parameters using process.argv
-pool
-  .query(
-    `
+// Query string
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name = $1'
+LIMIT $2
+`;
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
